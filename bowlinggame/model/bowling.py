db3909df35392e69cfc8622c41857f9de8b5ef90
Chucho <jehincapie@udem.edu.co>
2022-10-28 15:38:25 -0500

    Finish implementation and testing
    - Add additional instructions in README.MD

---
+++ bowlinggame/model/bowling.py
@@ -0,0 +1,117 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Roll:
+    pins: int
+
+
+class Frame:
+    def __init__(self):
+        self.rolls: list[Roll] = []
+        self._next_frame: Optional[Frame] = None
+
+    @property
+    def next_frame(self):
+        return self._next_frame
+
+    @next_frame.setter
+    def next_frame(self, value):
+        self._next_frame = value
+
+    @property
+    def total_pins(self) -> int:
+        return sum(roll.pins for roll in self.rolls)
+
+    def add_roll(self, pins: int):
+        if pins + self.total_pins > 10:
+            raise ValueError("A frame's rolls cannot exceed 10 pins")
+
+        if len(self.rolls) < 2:
+            self.rolls.append(Roll(pins))
+
+    def is_strike(self):
+        return self.rolls[0].pins == 10
+
+    def is_spare(self):
+        return self.rolls[0].pins + self.rolls[1].pins == 10
+
+    def score(self) -> int:
+        points = self.total_pins
+        if self.is_strike():
+            if len(self.next_frame.rolls) == 2:
+                points += self.next_frame.total_pins
+            else:
+                points += self.next_frame.rolls[0].pins + self.next_frame.next_frame.rolls[0].pins
+        elif self.is_spare():
+            points += self.next_frame.rolls[0].pins
+
+        return points
+
+
+class TenthFrame(Frame):
+    def __init__(self):
+        super().__init__()
+        self.extra_roll: Optional[Roll] = None
+
+    def add_roll(self, pins: int):
+        if len(self.rolls) < 2:
+            self.rolls.append(Roll(pins))
+        elif len(self.rolls) == 2 and self.extra_roll is None:
+            if self.is_strike() or self.is_spare():
+                self.extra_roll = Roll(pins)
+            else:
+                raise IndexError("Can't throw bonus roll with an open tenth frame")
+        else:
+            raise IndexError("Can't add more than three rolls to the tenth frame")
+
+    def score(self) -> int:
+        points = self.total_pins
+        if self.is_strike() or self.is_spare():
+            return points + self.extra_roll.pins
+        return points
+
+
+class Game:
+
+    MAX_FRAMES = 10
+
+    def __init__(self):
+        self.frames: list[Frame] = []
+        self._init_frames()
+        self.roll_count: int = 0
+
+    @property
+    def current_frame(self) -> int:
+        if self.roll_count < (Game.MAX_FRAMES * 2):
+            return self.roll_count // 2
+        else:
+            return Game.MAX_FRAMES - 1
+
+    def _init_frames(self):
+        frame = Frame()
+
+        for i in range(9):
+            if i < 8:
+                next_frame = Frame()
+            else:
+                next_frame = TenthFrame()
+            frame.next_frame = next_frame
+            self.frames.append(frame)
+            frame = next_frame
+
+        self.frames.append(frame)
+
+    def roll(self, pins: int):
+        self.frames[self.current_frame].add_roll(pins)
+        if self.frames[self.current_frame].is_strike():
+            self.roll_count += 2
+        else:
+            self.roll_count += 1
+
+    def score(self) -> int:
+        if self.current_frame < Game.MAX_FRAMES - 1:
+            raise IndexError("There are not enough frame to calculate score")
+
+        return sum(frame.score() for frame in self.frames)
---
+++ tests/unit/test_bowling.py
@@ -0,0 +1,106 @@
+import pytest
+
+
+from bowlinggame.model.bowling import Game
+
+
+@pytest.fixture
+def game():
+    """ Return a new Game """
+    return Game()
+
+
+def roll_many(game, n, pins):
+    """
+    Simulates several roll in a game
+    :param game: the game object
+    :param n: the number of rolls
+    :param pins: the number of pins knocked down in every roll
+    """
+    for _ in range(n):
+        game.roll(pins)
+
+
+def roll_spare(game):
+    """
+    Simulates rolling a spare
+    :param game: the game object
+    """
+    game.roll(5)
+    game.roll(5)
+
+
+def roll_strike(game):
+    """
+    Simulates rolling a strike
+    :param game: the game object
+    """
+    game.roll(10)
+
+
+def test_awful_game(game):
+    """ Test a game where no pins where knocked down """
+    roll_many(game, 20, 0)
+    assert game.score() == 0
+
+
+def test_all_ones_game(game):
+    """ test a game where one pin was knocked down in all rolls """
+    roll_many(game, 20, 1)
+    assert game.score() == 20
+
+
+def test_one_spare(game):
+    """ Test a game with one spare """
+    roll_spare(game)
+    game.roll(3)
+    roll_many(game, 17, 0)
+    assert game.score() == 16
+
+
+def test_one_strike(game):
+    """ Test a game with one strike """
+    roll_strike(game)
+    game.roll(3)
+    game.roll(4)
+    roll_many(game, 16, 0)
+    assert game.score() == 24
+
+
+def test_perfect_game(game):
+    """ Test a game with all strikes """
+    roll_many(game, 12, 10)
+    assert game.score() == 300
+
+
+def test_raise_exception_when_frame_rolls_exceed_10_pins(game):
+    with pytest.raises(ValueError, match="A frame's rolls cannot exceed 10 pins"):
+        game.roll(5)
+        game.roll(7)
+
+
+def test_raise_exception_when_bonus_roll_with_open_tenth_frame(game):
+    roll_many(game, 18, 0)
+    with pytest.raises(IndexError, match="Can't throw bonus roll with an open tenth frame"):
+        game.roll(5)
+        game.roll(2)
+        game.roll(5)
+
+
+def test_raise_exception_when_tenth_frame_with_more_than_3_rolls(game):
+    roll_many(game, 12, 10)
+    with pytest.raises(IndexError, match="Can't add more than three rolls to the tenth frame"):
+        game.roll(5)
+
+
+def test_raise_exception_when_calculate_score_without_game_end(game):
+    roll_many(game, 10, 2)
+    with pytest.raises(IndexError, match="There are not enough frame to calculate score"):
+        game.score()
+
+
+
+
+
+
+
